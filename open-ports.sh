b00c8c4bd5b556355d0df19d9e169b57a045c200
Olivier Richaud <olivier@richaud.net>
2016-02-15 13:21:26 +0100

    First revision

--- open-ports.sh
+++ open-ports.sh
@@ -9,4 +9,6 @@ $VBOX_MANAGE controlvm dev natpf1 "couchbase-srv-2,tcp,127.0.0.1,11211,,11211"
 
 $VBOX_MANAGE controlvm dev natpf1 "docker-ui,tcp,127.0.0.1,9000,,9000"
 
+$VBOX_MANAGE controlvm dev natpf1 "cbserver,tcp,127.0.0.1,8080,,8080"
+
 exit 0
