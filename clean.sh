f3ee16e2405147e3fb99eb4f9045d0dd2137951b
Olivier Richaud <olivier@richaud.net>
2016-03-26 22:15:37 +0100

    removed useless network in docker configuration

--- clean.sh
+++ clean.sh
@@ -3,6 +3,5 @@
 source ./docker-env.sh
 
 $DOCKER rm --force $(docker ps -a -q)
-$DOCKER network rm cb-net
 
 exit 0
--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 source ./docker-env.sh
 
-$DOCKER network create -d bridge $NETWORK
-
 $DOCKER run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock \
 --net=$NETWORK --name DockerUI dockerui/dockerui
 
